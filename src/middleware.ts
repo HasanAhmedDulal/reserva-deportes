5b7748c697ed95e5dfb6330c8022317eb5d27d98
unknown <HasanAhmedDulal@HasanAhmedDulal>
2024-10-19 23:52:50 +0600

    all fix update

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export const middleware = async (request) => {
-    const token = cookies().get('next-auth.session-token');
+    const token = cookies().get('__Secure-next-auth.session-token');
 
 
     // if (!token) {
